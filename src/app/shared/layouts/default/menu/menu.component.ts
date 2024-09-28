6767961a89ce145308965eefabae96f5d0b4f779
Alfredo Ruiz <rruiz@tsspe.com>
2024-09-28 13:32:37 -0500

    SVG files are created
    Move svg content into files

--- src/app/shared/layouts/default/menu/menu.component.ts
+++ src/app/shared/layouts/default/menu/menu.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, output, signal } from '@angular/core';
+import { FileReaderService } from '../../../services/file-reader.service';
+import { SafeHtml } from '@angular/platform-browser';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -10,19 +13,53 @@ import { Component, output, signal } from '@angular/core';
 })
 export class MenuComponent {
   isOpenNav = signal(false);
-  mode = signal('light');
   changeStateNav = output<boolean>();
+  isDark = signal(false);
+
+  sunIcon = signal<SafeHtml>('');
+  moonIcon = signal<SafeHtml>('');
+  modeIcon = signal<SafeHtml>('');
+  bellIcon = signal<SafeHtml>('');
+  expandIcon = signal<SafeHtml>('');
+  homeIcon = signal<SafeHtml>('');
+  tableIcon = signal<SafeHtml>('');
+  graphIcon = signal<SafeHtml>('');
+
+  constructor(private fileReaderService: FileReaderService) {
+    forkJoin([
+      this.fileReaderService.readSVG('icons/sun.svg'),
+      this.fileReaderService.readSVG('icons/moon.svg'),
+      this.fileReaderService.readSVG('icons/bell.svg'),
+      this.fileReaderService.readSVG('icons/expand.svg'),
+      this.fileReaderService.readSVG('icons/home.svg'),
+      this.fileReaderService.readSVG('icons/table.svg'),
+      this.fileReaderService.readSVG('icons/graph.svg'),
+    ]).subscribe(([sun, moon, bell, expand, home, table, graph]) => {
+      this.sunIcon.set(sun);
+      this.moonIcon.set(moon);
+      this.modeIcon.set(moon);
+      this.bellIcon.set(bell);
+      this.expandIcon.set(expand);
+      this.homeIcon.set(home);
+      this.tableIcon.set(table);
+      this.graphIcon.set(graph);
+    });
+  }
 
   toggleNav() {
     this.isOpenNav.update(x => !x);
     this.changeStateNav.emit(this.isOpenNav());
   }
 
-  setMode(mode: 'dark' | 'light') {
-    this.mode.set(mode);
-    if (this.mode() === 'dark') 
+  toggleMode() {
+    this.isDark.update(x => !x);
+    if (this.isDark()) {
       document.documentElement.classList.add('dark');
-    else
+      this.modeIcon.set(this.sunIcon());
+    }
+    else {
       document.documentElement.classList.remove('dark');
+      this.modeIcon.set(this.moonIcon());
+    }
   }
 }
