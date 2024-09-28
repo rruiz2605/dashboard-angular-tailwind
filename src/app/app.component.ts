ecb3aeaf5470b845016b68e2bf99d3a4d3d73e71
Alfredo Ruiz <rruiz@tsspe.com>
2024-09-28 01:00:56 -0500

    Convert to angular page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dashboard';
+  isOpenNav = signal(false);
+  mode = signal('light');
 }
